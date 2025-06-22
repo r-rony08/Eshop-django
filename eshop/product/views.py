ed401dd5b38a4b8fd89c23dc8d0e3814706ad761
r-rony08 <robiul.rony1008@gmail.com>
2025-06-22 20:34:09 +0600

    ADD Filters

--- eshop/product/views.py
+++ eshop/product/views.py
@@ -3,14 +3,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from .filters import ProductFilter
 # Create your views here.
 
 @api_view(['GET'])
 def get_products(request):
 
-    products = Product.objects.all()
+    filterset = ProductFilter(request.GET, queryset=Product.objects.all().order_by('id'))
 
-    serializer = ProductSerializer(products, many=True)
+    serializer = ProductSerializer(filterset.qs, many=True)
 
     return Response({"products": serializer.data})
 
