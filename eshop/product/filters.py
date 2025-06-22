0e47b25644aede628fe55391c8bda8333e8e2f40
r-rony08 <robiul.rony1008@gmail.com>
2025-06-22 20:58:35 +0600

    Adding Product Search

--- eshop/product/filters.py
+++ eshop/product/filters.py
@@ -2,6 +2,11 @@ from django_filters import rest_framework as filters
 from .models import Product
 
 class ProductFilter(filters.FilterSet):
+
+    keyword = filters.CharFilter(field_name="name", lookup_expr="icontains")
+    initial_buying_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    initial_selling_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
+
     class Meta:
         model = Product
-        fields = ('Categories', 'brand')
+        fields = ('keyword', 'Categories', 'brand', 'initial_buying_price', 'initial_selling_price')
